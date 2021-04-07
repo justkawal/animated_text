c480704c148cb8775a8373d5ec28102ff562d477
Kawaljeet <49296873+justkawal@users.noreply.github.com>
2021-04-07 09:38:19 +0530

    changes

--- lib/src/animated_text.dart
+++ lib/src/animated_text.dart
@@ -141,9 +141,8 @@ class _AnimatedTextState extends State<AnimatedText>
   }
 
   bool _isSame(List<String> list1, List<String> list2) {
-    int list1Length = list1.length;
-    if (list1Length == list2.length) {
-      for (int i = 0; i < list1Length; i++) {
+    if (list1.length == list2.length) {
+      for (int i = 0; i < list1.length; i++) {
         if (list1[i] != list2[i]) {
           return false;
         }
